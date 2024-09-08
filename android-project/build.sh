a5207617b716639a55fe04865c85936e0a80bf50
Your Name <markus@pering.de>
2024-09-08 15:13:33 +0200

    updated build.sh

--- android-project/build.sh
+++ android-project/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=$HOME/Android/Sdk:$HOME/Android/Sdk/ndk/r26d:$HOME/Android/Sdk/platform-tools:$PATH
+export PATH=$HOME/Android/Sdk:$HOME/Android/Sdk/ndk/r27:$HOME/Android/Sdk/platform-tools:$PATH
 export ANDROID_HOME="$HOME/Android/Sdk"
 export JAVA_HOME="/usr/lib/jvm/java-1.17.0-openjdk-amd64"
 
