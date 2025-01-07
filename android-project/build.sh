5fb3636a0a5560d1fd6ed45db7769b5cd5f2229a
Farmer_Markus <145863173+Farmer-Markus@users.noreply.github.com>
2025-01-07 01:19:44 +0100

    updated submodules

--- android-project/build.sh
+++ android-project/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH=$HOME/Android/Sdk:$HOME/Android/Sdk/ndk/r27:$HOME/Android/Sdk/platform-tools:$PATH
-export ANDROID_HOME="$HOME/Android/Sdk"
-export JAVA_HOME="/usr/lib/jvm/java-1.17.0-openjdk-amd64"
+export ANDROID_HOME="$HOME/Android"
+export JAVA_HOME="/usr/lib/jvm/java-17-openjdk"
 
 ./gradlew $1 $2 $3 $4 $5
