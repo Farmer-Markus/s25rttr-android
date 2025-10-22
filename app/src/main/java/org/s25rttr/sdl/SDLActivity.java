6ecbc5bdf0b5cb2a1f7336ad246224f921338d1d
Farmer-Markus <145863173+Farmer-Markus@users.noreply.github.com>
2025-10-23 00:25:03 +0200

    Changed some fullscreen behavior

--- app/src/main/java/org/s25rttr/sdl/SDLActivity.java
+++ app/src/main/java/org/s25rttr/sdl/SDLActivity.java
@@ -2,25 +2,9 @@ package org.s25rttr.sdl;
 
 import android.content.pm.ActivityInfo;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
 
 public class SDLActivity extends org.libsdl.app.SDLActivity {
-    private int windowFlags =
-            View.SYSTEM_UI_FLAG_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE;// | View.INVISIBLE;
-
-    // Overrides SDL's internal orientation system to force custom user orientations
-    /*@Override
-    public void setOrientationBis(int w, int h, boolean resizable, String hint) {
-        mSingleton.setRequestedOrientation(getIntent().getIntExtra("rotation", ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE));
-    }*/
-
-    // Above approach would cause issues when changing to windowed mode
     @Override
     public void setOrientationBis(int w, int h, boolean resizable, String hint) {
         super.setOrientationBis(w, h, resizable, hint);
