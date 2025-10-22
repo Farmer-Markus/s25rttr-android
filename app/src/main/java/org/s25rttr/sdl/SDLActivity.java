5fee1c43d16b81b556e413a82da5001d6aff7b3c
Farmer-Markus <145863173+Farmer-Markus@users.noreply.github.com>
2025-10-23 00:23:13 +0200

    Changed some fullscreen behavior

--- app/src/main/java/org/s25rttr/sdl/SDLActivity.java
+++ app/src/main/java/org/s25rttr/sdl/SDLActivity.java
@@ -6,6 +6,13 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class SDLActivity extends org.libsdl.app.SDLActivity {
+    private int windowFlags =
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE;// | View.INVISIBLE;
 
     // Overrides SDL's internal orientation system to force custom user orientations
     /*@Override
@@ -34,19 +41,42 @@ public class SDLActivity extends org.libsdl.app.SDLActivity {
         super.onResume();
 
         if(mFullscreenModeActive)
-            setFullScreen();
+            tryEnableFullscreen(5);
     }
 
-    private void setFullScreen() {
-        Window window = getWindow();
-        int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
-        window.getDecorView().setSystemUiVisibility(flags);
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if(mFullscreenModeActive)
+            tryEnableFullscreen(5);
+    }
+
+    public void tryEnableFullscreen(final int maxTries) {
+        tryEnableFullscreen(0, maxTries);
+    }
+
+    private void tryEnableFullscreen(final int currTry, final int maxTries) {
+        final View view = getWindow().getDecorView();
+        view.postDelayed(()->{
+            view.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+
+            // Did it work?
+            int flags = view.getSystemUiVisibility();
+            boolean finished = (flags & View.SYSTEM_UI_FLAG_FULLSCREEN) != 0;
+            
+            if(!finished && currTry < maxTries) {
+                // start another instance
+                tryEnableFullscreen(currTry + 1, maxTries);
+            }
+            
+        }, 500);
     }
 }
